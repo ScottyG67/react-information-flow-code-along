8ed76fccf96bea548d7675aefaf8212dac94ceae
github username <scott.gloyna01@gmail.com>
2021-03-15 15:38:04 -0500

    Done.

--- src/Parent.js
+++ src/Parent.js
@@ -7,15 +7,25 @@ class Parent extends Component {
   constructor() {
     super()
     this.state = {
-      color: getRandomColor()
+      color: getRandomColor(),
+      childColor1: "#FFF",
+      childColor2: "#FFF"
     }
+  }
+
+  changeColor = (newChildColor1,newChildColor2) => {
+    this.setState({
+      color: getRandomColor(),
+      childColor1: newChildColor1,
+      childColor2: newChildColor2
+    })
   }
 
   render() {
     return (
       <div className="parent" style={{backgroundColor: this.state.color}}>
-        <Child />
-        <Child />
+        <Child handleColorChange = {this.changeColor} color ={this.state.childColor1}/>
+        <Child handleColorChange = {this.changeColor} color ={this.state.childColor2}/>
       </div>
     )
   }
